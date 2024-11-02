1d985ef039bea7349952794cbf7cea1970fd9e22
tarynblakemiller <yourfriendtaryn@gmail.com>
2024-11-02 18:41:57 -0400

    client side and board view render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,32 @@ if (typeof global === "undefined") {
 }
 import { Fragment } from "react";
 import "vibe-storybook-components/index.css";
-// import mondaySdk from "monday-sdk-js";
-// import "./App.css";
+import mondaySdk from "monday-sdk-js";
+import "monday-ui-react-core/dist/main.css";
 import { OrderForm } from "./components/Order/OrderForm/OrderForm";
+import { useEffect, useState } from "react";
+import { AttentionBox } from "monday-ui-react-core";
+
+const monday = mondaySdk();
 
 function App() {
+  const [context, setContext] = useState();
+
+  useEffect(() => {
+    // Notice this method notifies the monday platform that user gains a first value in an app.
+    // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
+    monday.execute("valueCreatedForUser");
+
+    // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
+    monday.listen("context", (res) => {
+      setContext(res.data);
+    });
+  }, []);
+
+  // const attentionBoxText = `Hello, your user_id is: ${
+  //   context ? context.user.id : "still loading"
+  // }.`;
+
   return (
     <Fragment>
       <OrderForm />
--- client/src/components/Order/OrderForm/OrderForm.jsx
+++ client/src/components/Order/OrderForm/OrderForm.jsx
@@ -2,8 +2,12 @@ import BaseInput from "../../Input/Input";
 import OrderButton from "../../Button/Button";
 import { useState } from "react";
 import FragranceSelector from "../../Dropdown/Dropdown";
+import PropTypes from "prop-types";
+import { Icon } from "monday-ui-react-core";
+import { Heading } from "monday-ui-react-core/next";
+import "./OrderForm.css";
 
-export const OrderForm = ({ onSubmit }) => {
+export const OrderForm = ({ onSubmit, boardId }) => {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -17,31 +21,65 @@ export const OrderForm = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <BaseInput
-        title={"First Name"}
-        placeholder="John"
-        size="small"
-        value={formData.firstName}
-        onChange={(value) => setFormData({ ...formData, firstName: value })}
-      />
-      <BaseInput
-        title="Last Name"
-        placeholder="Doe"
-        size="small"
-        value={formData.lastName}
-        onChange={(value) => setFormData({ ...formData, lastName: value })}
-      />
-      <BaseInput
-        type="number"
-        title="Quantity"
-        placeholder="0"
-        size="small"
-        value={formData.quantity}
-        onChange={(value) => setFormData({ ...formData, quantity: value })}
-      />
-      <FragranceSelector />
-      <OrderButton type="submit" text="Start Order" />
-    </form>
+    <div className="order-maker-container">
+      <div>
+        <div className="order-maker-header">
+          <Heading type="h2">Order Maker</Heading>
+          <button className="text-gray-400">⋮</button>
+          <Icon clickable="true" icon={function noRefCheck() {}} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-grid-container">
+            <div className="input-item">
+              <BaseInput
+                title="First Name"
+                placeholder="Joseph"
+                size="small"
+                value={formData.firstName}
+                onChange={(value) =>
+                  setFormData({ ...formData, firstName: value })
+                }
+              />
+            </div>
+            <div className="input-item">
+              <BaseInput
+                title="Last Name"
+                placeholder="Smith"
+                size="small"
+                value={formData.lastName}
+                onChange={(value) =>
+                  setFormData({ ...formData, lastName: value })
+                }
+              />
+            </div>
+            <div className="input-item">
+              <BaseInput
+                type="number"
+                title="Quantity"
+                placeholder="0"
+                size="small"
+                value={formData.quantity}
+                onChange={(value) =>
+                  setFormData({ ...formData, quantity: value })
+                }
+              />
+            </div>
+          </div>
+          <FragranceSelector />
+          <div className="fragrance-selector-container"></div>
+          <div className="button-container">
+            <OrderButton
+              className="order-button"
+              type="submit"
+              text="Start Order"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   );
+};
+
+OrderForm.propTypes = {
+  onSubmit: PropTypes.func,
 };
